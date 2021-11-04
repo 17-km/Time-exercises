81edd2880b63735da2d43428a6b74a928efc30ff
17-km <krystian.mikoda.programista@gmail.com>
2021-11-04 01:30:59 +0100

    Added date getting from system

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
+struct Date
+{
+    int year;
+    int month;
+    int day;
+};
+
+Date getTodaysDateFromSystem()
+{
+    Date todaysDate;
+    time_t now = time(0);
+    tm* localTime = localtime(&now);
+    todaysDate.year = 1900 + localTime->tm_year;
+    todaysDate.month = localTime->tm_mon;
+    todaysDate.day = localTime->tm_mday;
+    return todaysDate;
+}
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    Date todaysDate = getTodaysDateFromSystem();
+    cout << todaysDate.year << "-" << todaysDate.month << "-" << todaysDate.day << endl;
     return 0;
 }
